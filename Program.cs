0ec1dd515df74b93287bc58899e4d227b71257f6
besatb21 <bjacaj19@epoka.edu.al>
2022-11-14 01:04:04 +0100

    Generated Controllers using dotnet-aspnet-generator

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
+
+using LibraryApp.Models;
+using LibraryApp.Controllers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BookContext>(opt =>
+    opt.UseInMemoryDatabase("BookList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,6 +30,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/author", ()=>"");
 
 app.Run();
 
